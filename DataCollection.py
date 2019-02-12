6c959c78381faba1dbe899f4a73bdd2918796cd1
Tom Marsh <tom.marsh@adscale.co.nz>
2019-02-12 19:49:44 +1300

    Started on fetching item ids and deciding if they are promising. Note: The API calls are slow

--- DataCollection.py
+++ DataCollection.py
@@ -1,14 +1,23 @@
 
-from requests import get
+# Libraries
 import csv
 import datetime
+import math
+import logging
+
+import pandas as pd
+
+from requests import get
+from time import sleep
+
+
+logger = logging.getLogger("OSRS")
 
 
 def collect_item_graph_data_and_write_to_csv(base_url, endpoint, item_id, file_name):
     # Setup
     url = base_url + endpoint.format(item_id)
-
-    print(url)
+    print("Collecting item graph data from: {}".format(url))
 
     # Collect
     response = get(url).json()
@@ -31,5 +40,67 @@ def collect_item_graph_data_and_write_to_csv(base_url, endpoint, item_id, file_n
                              "Item Value Daily": value_daily,
                              "Item Value Average": value_average
                              })
+
+
+def collect_item_ids(base_url, endpoint_catalogue_category, endpoint_catalogue_items, file_name):
+    # Sift through category data
+    url_category = base_url + endpoint_catalogue_category
+    print("Collecting catalogue category data from: {}".format(url_category))
+    response_category = get(url_category).json()
+
+    with open(file_name, "w", newline="") as csvfile:
+        field_names = ["Item Id", "Item Name"]
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+
+        data_alpha = response_category["alpha"]
+        for entry in data_alpha:
+            letter = entry["letter"]
+            items = entry["items"]
+
+            # The API cannot parse # so use %23
+            if letter == "#":
+                letter = "%23"
+
+            # If there are multiple pages iterate over them
+            for page in range(1, int(math.ceil(items / 12.0))):
+                url_items = base_url + endpoint_catalogue_items.format(letter, page)
+                print("Collecting catalogue item data from: {}".format(url_items))
+                response_items = get(url_items).json()
+
+                # Sift through each item and glean the id
+                for item in response_items["items"]:
+                    writer.writerow({"Item Id": item["id"],
+                                     "Item Name": item["name"]
+                                     })
+
+                # Wait for a little between API calls
+                sleep(2)
+
+
+def collect_promising_items(base_url, df_items: pd.DataFrame, endpoint_catalogue_detail):
+    promising = dict()
+
+    for item_id, item_name in df_items.iterrows():
+
+        url_item = base_url + endpoint_catalogue_detail.format(item_id)
+        print("Determining if item: {} [{}] looks promising".format(item_name, item_id))
+        response_item = get(url_item).json()
+
+        item_detail = response_item["item"]
+        current = item_detail["current"]
+        day30 = item_detail["day30"]
+
+        if "positive" in [current["trend"], day30["trend"]]:
+            promising[item_name] = {
+                "id": item_id,
+                "trend": day30["change"],
+                "members": item_detail["members"]
+            }
+
+        sleep(0.5)
+
+    return promising
+
 
 
--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 
-# Libraries]
+# Libraries
 import warnings
 import pandas as pd
 import statsmodels.api as sm
 
 import itertools
+import logging
 import matplotlib
-from matplotlib import pyplot as plt
 
-from DataCollection import collect_item_graph_data_and_write_to_csv
-from Functions_Model import determine_best_p_d_q_variables
+from time import sleep
+from matplotlib import pyplot as plt
+from DataCollection import collect_item_graph_data_and_write_to_csv, collect_item_ids, collect_promising_items
 
 
 # =================================================
@@ -24,79 +25,62 @@ matplotlib.rcParams["text.color"] = "k"
 matplotlib.rcParams["figure.figsize"] = 18, 8
 
 
+# =================================================
+#     ========== STEP ONE : SETUP ==========
+# =================================================
+
 # Important
 BASE_URL = "http://services.runescape.com/m=itemdb_oldschool"
-
+logger = logging.getLogger("OSRS")
+logger.setLevel("DEBUG")
 
 # Endpoints
-ITEM_DETAIL = "/api/catalogue/detail.json?item={}"
-ITEM_GRAPHS = "/api/graph/{}.json"
-
-# Item dict
-items = {"Bow string": 1777}
-
-
-# =================================================
-#     ========== STEP ONE : GET DATA ==========
-# =================================================
-file_name = "test.csv"
-collect_item_graph_data_and_write_to_csv(BASE_URL, ITEM_GRAPHS, 1777, file_name)
-df = pd.read_csv(file_name)
+ENDPOINT_CATALOGUE_CATEGORY = "/api/catalogue/category.json?category=1"
+ENDPOINT_CATALOGUE_ITEMS = "/api/catalogue/items.json?category=1&alpha={}&page={}"
+ENDPOINT_CATALOGUE_DETAIL = "/api/catalogue/detail.json?item={}"
+ENDPOINT_GRAPHS = "/api/graph/{}.json"
 
 
 # =================================================
-#   ========== STEP TWO : PROCESS DATA ==========
+#     ========== STEP TWO : DISCOVERY ==========
 # =================================================
-df = df.sort_values("Timestamp")
-df = df.set_index("Timestamp")
-df.index = pd.to_datetime(df.index)
-
-freq = 7
-sample_data = df["Item Value Daily"].resample(str(freq) + "D").mean()
-decomposition = sm.tsa.seasonal_decompose(sample_data, freq=freq)
-decomposition.plot()
 
 
-# =================================================
-# ========== STEP THREE : MODEL CREATION ==========
-# =================================================
-# ARIMA stuff
-arima_pdq = determine_best_p_d_q_variables(sample_data)
-arima_model = arima_pdq.fit(disp=False)
-# print(arima_model.summary().tables[1])
-# arima_model.plot_diagnostics(figsize=(16, 8))
+# Collect item ids
+file_name = "ItemIds.csv"
+collect_item_ids(BASE_URL, ENDPOINT_CATALOGUE_CATEGORY, ENDPOINT_CATALOGUE_ITEMS, file_name)
+
+df_items = pd.read_csv(file_name)
+promising_items = collect_promising_items(BASE_URL, df_items, ENDPOINT_CATALOGUE_DETAIL)
+
+print(promising_items)
 
 
-# =================================================
-#   ========== STEP FOUR : PREDICTIONS ==========
-# =================================================
-# ARIMA stuff
-length = len(df.index) / 7
-# start_pred = pd.Timestamp.now().date() - pd.Timedelta(days=30)
-# end_pred = pd.Timestamp.now().date()
-arima_prediction = arima_model.get_prediction(start=int(length * 0.8), end=int(length + 10), dynamic=False)
-arima_prediction_ci = arima_prediction.conf_int()
-
-ax = df.plot(label="Observed")
-arima_prediction.predicted_mean.plot(ax=ax, label="One-step ahead Forecast", alpha=0.8)
-
-ax.fill_between(arima_prediction_ci.index,
-                arima_prediction_ci.iloc[:, 0],
-                arima_prediction_ci.iloc[:, 1], color='k', alpha=0.2)
-ax.set_xlabel("Timestamp")
-ax.set_ylabel("Closing Price")
-plt.legend()
 
 
-# =================================================
-#    ========== STEP FIVE : EVALUATION ==========
-# =================================================
-# ARIMA MSE
-# arima_forecast = arima_prediction.predicted_mean[:(end_pred - pd.DateOffset(minutes=10))]
-# arima_truth = sample_data[start_pred:]
-# mse = ((arima_forecast - arima_truth) ** 2).mean()
-# print("The Mean Square Error of our forecast is {}".format(round(mse, 2)))
 
-plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
